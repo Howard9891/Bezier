028d9fa2f1c07c1ace9c9b29304cf8a0b0866c53
Howard9891 <Howard9891@163.com>
2016-06-25 12:29:07 +0800

    init commit

---
+++ src/main/java/com/iyuba/myapplication/BezierView.java
@@ -0,0 +1,55 @@
+package com.iyuba.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+/**
+ * 作者：mingyu on 16-6-25 11:30
+ * <p/>
+ * 邮箱：Howard9891@163.com
+ */
+public class BezierView extends View {
+
+    private Paint paint;
+
+    public BezierView(Context context) {
+        this(context,null);
+        Log.e("BezierView","1");
+    }
+
+    public BezierView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+        Log.e("BezierView","2");
+    }
+
+    public BezierView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        Log.e("BezierView","3");
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.FILL);
+        Log.e("BezierView","draw");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.e("BezierView","draw");
+        Path path = new Path();
+        path.moveTo(200,200);
+        path.quadTo(250,250,700,700);
+        canvas.drawPath(path,paint);
+
+
+    }
+}
